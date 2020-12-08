2c97ccdc712d67b41d972b92cd32cc75ff64d6e0
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2020-12-08 21:43:36 +0200

    Add ability to add ships (fully working)

--- main.cpp
+++ main.cpp
@@ -39,6 +39,7 @@ struct Ship {
     const int size;
     Coordinates coords;
     Ship(int _size) : size(_size) {}
+    Ship(int _size, Coordinates _coords) : size(_size), coords(_coords) {}
 };
 
 class Player {
@@ -51,6 +52,8 @@ class Player {
     void drawBoard();
 
     void addShip();
+    void addShipAtLocation(int);
+    bool tryPlacingShip(int, int, int, char);
     void editShip();
     void resetBoard();
     void commitBoard();
@@ -58,11 +61,14 @@ class Player {
     void clearScreen();
 
   private:
-    // 0 -> water, 1-> hit water, 2 -> ship, 3 -> sunken ship
+    // 0 -> water, 1-> ship, 2 -> hit water, 3 -> sunken ship
     unsigned short board[BOARD_SIZE][BOARD_SIZE] = {};
-    std::vector<std::vector<Ship>> ships = {
+    std::vector<std::vector<Ship>> remainingShips = {
         std::vector<Ship>(4, Ship(2)), std::vector<Ship>(3, Ship(3)),
         std::vector<Ship>(2, Ship(4)), std::vector<Ship>(1, Ship(6))};
+    std::vector<std::vector<Ship>> ships = {
+        std::vector<Ship>(), std::vector<Ship>(), std::vector<Ship>(),
+        std::vector<Ship>()};
     std::string playerName;
 };
 
@@ -82,14 +88,14 @@ int main() {
 
 Player::Player() {
     // Blank constructor
-    board[4][4] = 2;
-    board[4][5] = 3;
-    board[4][6] = 3;
-    board[4][7] = 2;
-    board[2][3] = 1;
-    board[1][2] = 2;
-    board[2][2] = 2;
-    board[3][2] = 2;
+    // board[4][4] = 1;
+    // board[4][5] = 3;
+    // board[4][6] = 3;
+    // board[4][7] = 1;
+    // board[2][3] = 2;
+    // board[1][2] = 1;
+    // board[2][2] = 1;
+    // board[3][2] = 1;
 }
 
 Player::Player(std::string path) {
@@ -97,7 +103,7 @@ Player::Player(std::string path) {
 }
 
 void Player::initBoard() {
-    initPlayer();
+    // initPlayer(); //FIXME
     char option;
     bool wasInvalid = false;
     do {
@@ -140,7 +146,7 @@ void Player::initBoard() {
 void Player::initPlayer() {
     clearScreen();
     std::cout << std::endl << "Choose player name:" << std::endl;
-    std::cin >> playerName; // FIXME
+    std::cin >> playerName;
 }
 
 // ╚ ╔ ╩ ╦ ╠ ═ ╬ ╣ ║ ╗ ╝
@@ -165,7 +171,7 @@ void Player::initPlayer() {
 void Player::drawBoard() {
     char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                       'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
-    char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char digits[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'X'};
     // First line - above LETTERS
     printLine(BOARD_SIZE + 1, "═══", "╔", "╦", "╗");
 
@@ -190,12 +196,12 @@ void Player::drawBoard() {
                 // rowBuffer[col] = "   ";
                 break;
             case 1: // hit water
-                rowBuffer[col] = " \033[1mX\033[0m ";
-                // rowBuffer[col] = " X ";
-                break;
-            case 2: // ship
                 rowBuffer[col] = "\033[7;1m   \033[0m";
                 // rowBuffer[col] = " ◯ ";
+                break;
+            case 2: // ship
+                rowBuffer[col] = " \033[1mX\033[0m ";
+                // rowBuffer[col] = " X ";
                 break;
             case 3: // sunken ship
                 rowBuffer[col] = "\033[7;1m X \033[0m";
@@ -219,29 +225,24 @@ void Player::addShip() {
     // add ship
     char option;
     bool wasInvalid = false;
+    std::string errorMessage;
     do {
         clearScreen();
         drawBoard();
         std::cout << std::endl
                   << "Select which ship to add:" << std::endl
                   << std::endl;
-        for (size_t i = 0, count; i < ships.size(); ++i) {
-            count = 0;
-            for (int j = 0; j < ships[i].size(); ++j) {
-                if (!ships[i][j].coords.areSet()) {
-                    ++count;
-                }
-            }
-
+        for (size_t i = 0, count; i < remainingShips.size(); ++i) {
+            count = remainingShips[i].size();
             if (count == 0) {
                 invertColours();
             }
 
             std::cout << (i + 1) << ". ";
-            printChar(ships[i][0].size, 'X');
+            printChar(remainingShips[i][0].size, 'X');
 
-            std::cout << std::setw(15 - ships[i][0].size) << count << " left"
-                      << std::endl;
+            std::cout << std::setw(15 - remainingShips[i][0].size) << count
+                      << " left" << std::endl;
 
             if (count == 0) {
                 revertColours();
@@ -250,15 +251,154 @@ void Player::addShip() {
 
         std::cout << "0. Go back to main menu" << std::endl << std::endl;
         if (wasInvalid) {
-            std::cout << "Invalid option \"" << option << "\" selected"
-                      << std::endl
+            std::cout << "Invalid option \"" << option << "\" selected "
+                      << errorMessage << std::endl
                       << std::endl;
         }
         wasInvalid = false;
         std::cin >> option;
         std::cin.clear();
 
+        if (option == '0') {
+            return;
+        }
+
+        if (option - '1' < 0 || option - '1' > (int)remainingShips.size()) {
+            std::cout << "Invalid option selected" << std::endl;
+            wasInvalid = true;
+            continue;
+        }
+
+        for (int i = 0; i < (int)remainingShips.size(); ++i) {
+            if (i == option - '1') {
+                if (remainingShips[i].size() == 0) {
+                    wasInvalid = true;
+                    errorMessage = "No more ships of that kind";
+                    continue;
+                }
+                addShipAtLocation(i);
+                continue;
+            }
+        }
     } while (true);
+}
+
+void Player::addShipAtLocation(int index) {
+    char option[3];
+    int row, col;
+    char direction;
+    bool wasInvalid = false;
+    std::string errorMessage;
+    bool success;
+    do {
+        clearScreen();
+        drawBoard();
+        std::cout << std::endl
+                  << "Select where to add a ship with length "
+                  << remainingShips[index][0].size
+                  << " (in format A1R [Column A-J, Row 1-X, Direction "
+                     "Right/Left]) or 0 to go back:"
+                  << std::endl
+                  << std::endl;
+
+        if (wasInvalid) {
+            std::cout << "Invalid option \"" << option << "\" selected "
+                      << errorMessage << std::endl
+                      << std::endl;
+        }
+        wasInvalid = false;
+
+        std::cin >> option;
+        std::cin.clear();
+
+        if (option[1] == '\0' && option[0] == '0') {
+            return;
+        }
+
+        col = option[0] - 'A';
+        if ((col < 0) || (col >= BOARD_SIZE)) {
+            errorMessage = "Invalid column selected";
+            wasInvalid = true;
+            continue;
+        }
+
+        row = option[1] == 'X' ? BOARD_SIZE - 1 : option[1] - '1';
+        if ((row < 0) || (row >= BOARD_SIZE)) {
+            errorMessage = "Invalid row selected";
+            wasInvalid = true;
+            continue;
+        }
+
+        direction = option[2];
+        if (direction != 'R' && direction != 'D') {
+            errorMessage = "Invalid direction selected";
+            wasInvalid = true;
+            continue;
+        }
+
+        success = tryPlacingShip(index, col, row, direction);
+
+        if (success) {
+            std::cout << "Successfully added a ship" << std::endl;
+            return;
+        } else {
+            wasInvalid = true;
+            errorMessage = "Cant place a ship at these coordinates";
+            continue;
+        }
+
+    } while (true);
+}
+
+bool Player::tryPlacingShip(int index, int col, int row, char direction) {
+
+    int size = remainingShips[index][0].size;
+
+    int rowOffset = 0, colOffset = 0;
+
+    switch (direction) {
+    case 'D':
+        rowOffset = 1;
+        break;
+    case 'R':
+        colOffset = 1;
+        break;
+    }
+
+    if (row + size * rowOffset > BOARD_SIZE ||
+        col + size * colOffset > BOARD_SIZE) {
+        return false;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (board[row + rowOffset * i][col + colOffset * i] != 0) {
+            return false;
+        }
+    }
+
+    for (int i = row - 1; i <= row + 1 + rowOffset * size; ++i) {
+        if ((i < 0) || (i >= BOARD_SIZE)) {
+            continue;
+        }
+        for (int j = col - 1; j <= col + 1 + colOffset * size; ++j) {
+            if ((j < 0) || (j >= BOARD_SIZE)) {
+                continue;
+            }
+
+            if (board[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        board[row + rowOffset * i][col + colOffset * i] = 1;
+    }
+
+    remainingShips[index].pop_back();
+    ships[index].push_back(Ship(size, Coordinates(row, col)));
+
+    return true;
 }
 
 void Player::editShip() {
